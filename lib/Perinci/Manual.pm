5b1e609bd61d0096fda4b73847a20217fb135c98
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-16 07:59:14 +0700

    Initial commit

---
+++ lib/Perinci/Manual.pm
@@ -0,0 +1,25 @@
+package Perinci::Manual;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Extra documentation for Perinci
+
+=head1 DESCRIPTION
+
+The C<Perinci> namespace contains several separate frameworks (for example:
+L<Perinci::Sub::Wrapper>, L<Perinci::CmdLine>), all themed around the L<Rinci>
+and L<Riap> specification, and each framework having its own documentation.
+
+This distribution contains additional, mostly cross-framework, and heavily
+task-oriented documentation.
+
+
+=head1 SEE ALSO
+
+L<Perinci>
+
+L<Rinci>, L<Riap>
